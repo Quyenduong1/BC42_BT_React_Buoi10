b9a5b277db6fde5ad6c1bf43cfea99869fba5ab6
Quyen <dgiaquyen40@gmail.com>
2023-04-16 22:41:16 +0700

    dang lam

---
+++ src/5_MovieSeat/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.scss'
+function Header() {
+  return (
+    <div className='title'>
+        <h1>Đặt Vé Xem Phim </h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/5_MovieSeat/MovieSeat.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+function MovieSeat() {
+  return (
+    <div className='container-fluid'>
+        <Header/>
+        <h1>MovieSeat</h1>
+    </div>
+  )
+}
+
+export default MovieSeat
